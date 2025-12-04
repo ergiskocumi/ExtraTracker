f8ab5041f4869edf570d2c96daac1bd4b6194396
Ergis_DELL <ergis.kocumi.majo@gmail.com>
2025-12-04 15:42:07 +0100

    aggiunta delle api al server di backend aggiunta del campo description

--- extra-tracker/server/index.js
+++ extra-tracker/server/index.js
@@ -6,9 +6,14 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiRoutes = require('./routes/api');
+
 // 1. MIDDLEWARE 
 app.use(cors()); // permetti richieste da altri domini
 app.use(express.json()); // permettere di leggere i JSON che ci manda react 
+
+app.use('/api', apiRoutes); // usa le rotte definite in routes/api.js
+
 
 // 2. CONNESSIONE AL DATABASE
 const MONGO_URI = 'mongodb://admin:password123@localhost:27017/extra-tracker?authSource=admin'; //TODO: spostare in variabile d'ambiente per non avere hardcoded le credenziali
